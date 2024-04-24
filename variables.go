a9e2d822ad645b968a2c958bfe426f41fdef5655
Harpreet Singh <harpreet.hs.2001@gmail.com>
2024-04-24 23:00:23 +0530

    small changes

---
+++ variables.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+	var intege1 int = 98
+	var floatvalue float64 = 98.8
+	var boolean bool = true
+	var name string =  "harpreet singh"
+	fmt.Printf("%v %f %v %q\n" ,intege1,floatvalue,boolean,name)
+}
